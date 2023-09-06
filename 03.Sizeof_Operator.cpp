8206b47f62eae4884556423d26a598cd472fdb40
rohan <ping.techyrohan@gmail.com>
2023-09-06 15:10:45 +0545

    basic intro

---
+++ 03.Sizeof_Operator.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;    
+
+int main() {
+    cout << sizeof(char) << endl;
+    cout << sizeof(int) << endl;
+    cout << sizeof(float) << endl;
+    cout << sizeof(double) << endl;
+    cout << sizeof(bool) << endl;
+    return 0;
+}
