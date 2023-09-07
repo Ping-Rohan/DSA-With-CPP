97deff386d9456be965270256d5e567b20da6251
rohan <ping.techyrohan@gmail.com>
2023-09-07 12:19:06 +0545

    implemented with vector

--- 16.Dutch_Flag_0_1_2_Sort.cpp
+++ 16.Dutch_Flag_0_1_2_Sort.cpp
@@ -1,25 +1,25 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
 int main() {
-    int arr[] = {1,2,2,2,2,2,2,2,2,2,2,2,0};
-    int size = sizeof(arr) / sizeof(int);
-    int start = 0 , i = 0 , end = size-1;
+    vector<int>nums{2,0,2,1,1,0};
+    int start = 0 , i = 0 , end = nums.size()-1;
 
     while(i<=end && start<=end) {
-            if(arr[i] == 2) {
-                swap(arr[i] , arr[end]);
+            if(nums[i] == 2) {
+                swap(nums[i] , nums[end]);
                 end--;
-            } else if(arr[i] == 1) {
+            } else if(nums[i] == 1) {
                 i++;
             } else {
-                swap(arr[i] , arr[start]);
+                swap(nums[i] , nums[start]);
                 start++;
                 i++;
             }
     }
 
-    for(auto value:arr) {
+    for(auto value:nums) {
         cout << value << " ";
     }
 
